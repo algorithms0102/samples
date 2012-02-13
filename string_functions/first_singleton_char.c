229875233d9d5cd602b1f91def386f3e7d7dc44f
algorithms0102 <algorithms0102@qq.com>
2012-02-13 21:28:39 +0800

    test

---
+++ string_functions/first_singleton_char.c
@@ -0,0 +1,13 @@
+/**
+ * File Name:first_singleton_char.c
+ * Description: find the first non repeat char in a string
+ * Create Date:13-02-2012
+ * Last Modified:Mon Feb 13 21:27:27 2012
+ *
+ */
+
+#include <stdio.h>
+
+char first_singleton_char(char *str) {
+	return '\0';
+}
