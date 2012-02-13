c309ea48c44464a860e99861b082ae90d5d81258
algorithms <algorithms0102@qq.com>
2012-02-13 14:46:46 +0800

    add string functions

--- binary_search/binary_search.c
+++ binary_search/binary_search.c
@@ -2,32 +2,39 @@
  * File Name:binary_search.c
  * Description:
  * Create Date:08-02-2012
- * Last Modified:Wed 08 Feb 2012 07:15:30 PM CST
+ * Last Modified:Wed 08 Feb 2012 10:13:08 PM CST
  *
  */
 
 #include <stdio.h>
+/**
+ * @target the element your want to search
+ * @array {int} a sorted array in ascending order
+ * @n     int  the number of elements in the array
+ */
+int binary_search(int target, int array[], int n) {
+    int lower = 0;
+    int upper = n-1;
 
-int binary_search(int array[], int n, int value) {
-	int left = 0;
-	int right = n-1;
-
-	while (left <= right) {
-		int middle = left + ((right - left) >> 1);
-		if (array[middle] > value) {
-			right = middle - 1;
-		} else if (array[middle] < value) {
-			left = middle + 1;
-		} else {
-			return middle;
-		}
-	}
-	return -1;
+    while (lower <= upper) {
+        //use lower + (upper - lower) to prevent overflow
+        int midpoint = lower + ((upper - lower) >> 1);
+        if (target < array[midpoint]) {
+            //lower.....target....midpoint....upper
+            upper = midpoint - 1;
+        } else if (target > array[midpoint]) {
+            //lower....midpoint....target....upper
+            lower = midpoint + 1;
+        } else {
+            return midpoint;
+        }
+    }
+    return -1;
 }
 
 
 int main() {
-	int array[10] = {1,2,3,4,5,6,7,8,9,10};
-	printf("%d\n", binary_search(array,10,3));
-	return 0;
+    int array[10] = {1,2,3,4,5,6,7,8,9,10};
+    printf("%d\n", binary_search(3, array, 10));
+    return 0;
 }
