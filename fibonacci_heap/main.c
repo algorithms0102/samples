35ea31a7d93d0aa928e0db5c07e83f475c734eef
algorithms <algorithms0102@qq.com>
2012-02-15 19:09:04 +0800

    change

--- fibonacci_heap/main.c
+++ fibonacci_heap/main.c
@@ -2,7 +2,7 @@
  * File Name:main.c
  * Description:
  * Create Date:15-02-2012
- * Last Modified:Wed 15 Feb 2012 06:06:50 PM CST
+ * Last Modified:Wed 15 Feb 2012 07:08:53 PM CST
  *
  */
 
@@ -10,21 +10,22 @@
 
 
 struct fib_heap_node {
-    int key;
+    int key; //key
     int degree;
     fib_heap_node * left;
     fib_heap_node * right;
     fib_heap_node * parent;
     fib_heap_node * child;
-    int marked;
-};
+    int marked; //whether node x has lost a child since the last time x was 
+                //made the child of another child
+} fib_heap_node;
 
 struct fib_heap {
-    int key_num;
-    fib_heap *min;
-    int max_num_of_degree;
+    int n;      // the number of nodes currently in heap
+    fib_heap *min; // the root  of a tree containing the minimum key
+    int max_degree; //the maximum degree of any node
     fib_heap_node * * cons;
-}
+} fib_heap;
 
 
 fib_heap * new_fib_heap() {
@@ -32,6 +33,6 @@ fib_heap * new_fib_heap() {
     heap = (fib_heap *)malloc(sizeof(fib_heap));
     
     if (heap == NULL) {
-        
+        printf
     }
 }
