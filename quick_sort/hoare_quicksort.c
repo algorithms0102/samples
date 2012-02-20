4a43038fceec78fb334b6bf9ebecb32bc47c5d3e
algorithms0102 <algorithms0102@qq.com>
2012-02-20 23:31:31 +0800

    add hoare

---
+++ quick_sort/hoare_quicksort.c
@@ -0,0 +1,74 @@
+//
+//  main.c
+//  quick_sort
+//
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#include <stdio.h>
+
+void swap(int *a, int *b); 
+int quick_sort(int array[], int p, int r);
+int partition(int array[], int p, int q);
+
+/**
+ * inplace swap
+ * this is an application of the property that a ^ a = 0
+ * ^: exclusive-or
+ */
+    //assum initially:    *a = x,               *b = y
+    *a = *a ^ *b;// step1 *a = x ^ y,           *b = y
+    *b = *a ^ *b;// step2 *a = x ^ y,           *b =  x ^ y ^ y = x
+    *a = *a ^ *b;// step3 *a = x ^ y ^ x = y,   *b = y
+}
+
+int quick_sort(int array[], int p, int r) {
+    if (p < r) {
+        int q = partition(array, p, r);
+        quick_sort(array, p, q-1);
+        quick_sort(array, q+1, r);
+    }
+    return 0;
+}
+
+int partition(int array[], int p, int q) {
+    int x = array[p];
+    int i = p;
+    while (p < q) {
+        while (p < q && array[q] >= x) q--;
+        swap(&array[p], &array[q]);
+        while (p < q && array[p] <= x) p++;
+
+    }
+    for (int j = p + 1; j <= q; j++) {
+        if (array[j] <= x) {
+            i = i + 1;
+            int tmp = array[i];
+            array[i] = array[j];
+            array[j] = tmp;
+            
+        }
+    }
+    int tmp = array[p];
+    array[p] = array[i];
+    array[i] = tmp;
+    return i;
+}
+
+
+int main (int argc, const char * argv[])
+{
+
+    int array[100];
+    for (int i = 0; i < 100; i++) {
+        array[i] = 100 - i;
+    }
+    quick_sort(array, 0, 99);
+    for ( int i = 0; i < 100; i++) {
+        printf("%d ", array[i]);
+    }
+
+    printf("Hello, World!\n");
+    return 0;
+}
+
